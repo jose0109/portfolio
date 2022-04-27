88a8d504d3b8f8df32a24995b6ebcaf6aaefa034
marco4721 <marco.benitez@novateva.com>
2022-04-26 22:11:45 -0400

    fix somoe layout details

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
-import About from './Components/About/About';
-import Features from './Components/Features/Features';
-import Work from './Components/Work/Work';
-import Courses from './Components/Courses/Courses';
-import Testimonials from './Components/Testimonials/Testimonials';
-import CTA from './Components/CTA/CTA';
-import Footer from './Components/Footer/Footer'
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Features from "./Components/Features/Features";
+import Work from "./Components/Work/Work";
+import Courses from "./Components/Courses/Courses";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import CTA from "./Components/CTA/CTA";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <About title="Hi, I'm Jose" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet
+      <About
+        title="Hi, I'm Jose"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet
           posuere nunc. Morbi rutrum tortor mi, a vulputate nisi finibus in.
           Nullam fermentum, ipsum vitae tristique tincidunt, metus metus sodales
           elit, ac viverra justo sem sit amet quam."
-        />
+      />
       <Features />
       <Work />
-      <About title="Courses Taken" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet
+      <About
+        title="Courses Taken"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet
           posuere nunc. Morbi rutrum tortor mi, a vulputate nisi finibus in.
           Nullam fermentum, ipsum vitae tristique tincidunt, metus metus sodales
           elit, ac viverra justo sem sit amet quam."
-        />
+      />
       <Courses />
       <Testimonials />
       <CTA />
